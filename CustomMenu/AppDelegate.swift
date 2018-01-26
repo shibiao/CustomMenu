8711a980b1a6f4aade6e7dff85a22fcae03d6fa3
WalkWithWind <956035125@qq.com>
2018-01-26 08:46:51 +0800

    Initial Commit

---
+++ CustomMenu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CustomMenu
+//
+//  Created by sycf_ios on 2018/1/26.
+//  Copyright © 2018年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CustomMenu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CustomMenu
+//
+//  Created by sycf_ios on 2018/1/26.
+//  Copyright © 2018年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
